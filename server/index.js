bd2bccbb46fcc0609cac0b955266208ec48b7f0a
Tomas Gadliauskas <tgadliauskas@zendesk.com>
2019-05-14 11:45:43 +0200

    body parser

--- server/index.js
+++ server/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const userRoutes = require("./routes/user");
 const mysqlConnection = require("./connection");
+const bodyParser = require("body-parser");
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
