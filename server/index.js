ff827831c5603ca61428f67add1444d0dd73576c
Tomas Gadliauskas <tgadliauskas@zendesk.com>
2019-05-14 17:54:00 +0200

    Revert "api endpoints"
    This reverts commit 187bf7f526fff8bf06993e8a58fda9b97df60c44.

--- server/index.js
+++ server/index.js
@@ -1,21 +1,37 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/user");
+const mysqlConnection = require("./connection");
 const bodyParser = require("body-parser");
-const port = 3000;
-const { Storage } = require("@google-cloud/storage");
+const Promise = require("bluebird");
+const GoogleCloudStorage = Promise.promisifyAll(
+  require("@google-cloud/storage")
+);
 
-const storage = new Storage({
+const port = 3000;
+
+const storage = GoogleCloudStorage({
   projectId: "api-project-567469925348",
-  keyFilename: "key.json"
+  keyFilename: "keyfile.json"
 });
 
 var BUCKET_NAME = "holiday-house";
 // https://googlecloudplatform.github.io/google-cloud-node/#/docs/google-cloud/0.39.0/storage/bucket
 var myBucket = storage.bucket(BUCKET_NAME);
+
 // check if a file exists in bucket
 // https://googlecloudplatform.github.io/google-cloud-node/#/docs/google-cloud/0.39.0/storage/file?method=exists
+var file = myBucket.file("puppy.jpeg");
+file
+  .existsAsync()
+  .then(exists => {
+    if (exists) {
+      // file exists in bucket
+    }
+  })
+  .catch(err => {
+    return err;
+  });
 
 // // upload file to bucket
 // // https://googlecloudplatform.github.io/google-cloud-node/#/docs/google-cloud/0.39.0/storage/bucket?method=upload
@@ -31,12 +47,11 @@ var myBucket = storage.bucket(BUCKET_NAME);
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => res.send("Hello World!"));
+
 app.use("/user", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.listen(port, () => {
+  mysqlConnection.connect(() => console.log("connected"));
   console.log(`Example app listening on port ${port}!`);
 });
--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -1,70 +1,9 @@
 const express = require("express");
-const router = express.Router();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
-const connection = require("../../connection");
+const router = express.Router();
 
 router.get("/", (req, res) => {
-  try {
-    connection.query("SELECT * FROM users", (err, rows) => {
-      if (err) throw err;
-      res.send(rows);
-    });
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
-
-router.post("/register", (req, res) => {
-  try {
-    const { name, email, phone, password } = req.body;
-    const hashedPassword = bcrypt.hashSync(password, 10);
-    connection.execute(
-      "INSERT INTO `users` (`name`, `email`, `phone`, `password`) VALUES (?, ?, ?, ?);",
-      [name, email, phone, hashedPassword],
-      (err, response) => {
-        if (err || response.affectedRows < 1)
-          res.send({
-            ok: false,
-            msg: `Failed to register. ${err.message && err.message}`
-          });
-        const token = jwt.sign(user.email, process.env.SECRET);
-        res.send({ ok: true, token });
-        if (err) throw err;
-        res.send(rows);
-      }
-    );
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
-
-router.post("/login", (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    connection.execute(
-      "SELECT * FROM users WHERE email=?",
-      [email],
-      (err, rows) => {
-        console.log(rows);
-        const user = rows[0];
-        if (bcrypt.compareSync(password, user.password)) {
-          const token = jwt.sign(user.email, process.env.SECRET);
-          res.send({ ok: true, token });
-        } else {
-          res.send({ ok: false, msg: "Invalid login credentials" });
-        }
-        if (err) throw err;
-      }
-    );
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
+  res.send("User root route works!");
 });
 
 module.exports = router;
