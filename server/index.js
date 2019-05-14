92272c541e7d67d92919edab933c8d46da592b7f
Tomas Gadliauskas <tgadliauskas@zendesk.com>
2019-05-14 12:53:15 +0200

    gcp

--- server/index.js
+++ server/index.js
@@ -3,7 +3,47 @@ const app = express();
 const userRoutes = require("./routes/user");
 const mysqlConnection = require("./connection");
 const bodyParser = require("body-parser");
+const Promise = require("bluebird");
+const GoogleCloudStorage = Promise.promisifyAll(
+  require("@google-cloud/storage")
+);
+
 const port = 3000;
+
+const storage = GoogleCloudStorage({
+  projectId: "api-project-567469925348",
+  keyFilename: "keyfile.json"
+});
+
+var BUCKET_NAME = "holiday-house";
+// https://googlecloudplatform.github.io/google-cloud-node/#/docs/google-cloud/0.39.0/storage/bucket
+var myBucket = storage.bucket(BUCKET_NAME);
+
+// check if a file exists in bucket
+// https://googlecloudplatform.github.io/google-cloud-node/#/docs/google-cloud/0.39.0/storage/file?method=exists
+var file = myBucket.file("puppy.jpeg");
+file
+  .existsAsync()
+  .then(exists => {
+    if (exists) {
+      // file exists in bucket
+    }
+  })
+  .catch(err => {
+    return err;
+  });
+
+// // upload file to bucket
+// // https://googlecloudplatform.github.io/google-cloud-node/#/docs/google-cloud/0.39.0/storage/bucket?method=upload
+// let localFileLocation = "./public/images/zebra.gif";
+// myBucket.uploadAsync(localFileLocation, { public: true }).then(file => {
+//   // file saved
+// });
+
+// // get public url for file
+// var getPublicThumbnailUrlForItem = file_name => {
+//   return `https://storage.googleapis.com/${BUCKET_NAME}/${file_name}`;
+// };
 
 app.use(bodyParser.json());
 
