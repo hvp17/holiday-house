aef1ab4df8cfe8a18f14e3f45448b5c76f6280bf
Tomas Gadliauskas <tgadliauskas@zendesk.com>
2019-05-21 13:52:09 +0200

    front-signup

--- js/main/signup.js
+++ js/main/signup.js
@@ -1,73 +1,81 @@
-$(document).on('click', 'a[href="#finish"]', function () {
-    console.log('click');
-var test = $('#wizard').serialize()
-console.log(test)
-    $.ajax({
-        url: 'http://localhost:3000/user/register',
-        data: $('#wizard').serialize(),
-        method: 'POST',
-        dataType: 'JSON'
-    }).always(function (jData) {
-        console.log("jData ", jData);
+$(document).on("click", 'a[href="#finish"]', function() {
+  console.log("click");
+  const data = $("#wizard")
+    .serializeArray()
+    .reduce(function(m, o) {
+      m[o.name] = o.value;
+      return m;
+    }, {});
+  console.log(data);
+  $.ajax({
+    url: "http://localhost:3000/user/register",
+    data: $("#wizard").serialize(),
 
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    method: "POST",
+    dataType: "JSON"
+  }).always(function(jData) {
+    console.log("jData ", jData);
+  });
+});
 
 /******************************************************
  :: bootstrap
  *****************************************************/
-$(function () {
-    $("#wizard").steps({
-        headerTag: "h2",
-        bodyTag: "section",
-        transitionEffect: "fade",
-        enableAllSteps: true,
-        transitionEffectSpeed: 500,
-        labels: {
-            finish: "Submit",
-            next: "Forward",
-            previous: "Backward"
-        }
-    });
-    $('.wizard > .steps li a').click(function () {
-        $(this).parent().addClass('checked');
-        $(this).parent().prevAll().addClass('checked');
-        $(this).parent().nextAll().removeClass('checked');
-    });
-    // Custome Jquery Step Button
-    $('.forward').click(function () {
-        $("#wizard").steps('next');
-    })
-    $('.backward').click(function () {
-        $("#wizard").steps('previous');
-    })
-    // Select Dropdown
-    $('html').click(function () {
-        $('.select .dropdown').hide();
-    });
-    $('.select').click(function (event) {
-        event.stopPropagation();
-    });
-    $('.select .select-control').click(function () {
-        $(this).parent().next().toggle();
-    })
-    $('.select .dropdown li').click(function () {
-        $(this).parent().toggle();
-        var text = $(this).attr('rel');
-        $(this).parent().prev().find('div').text(text);
-    })
-})
+$(function() {
+  $("#wizard").steps({
+    headerTag: "h2",
+    bodyTag: "section",
+    transitionEffect: "fade",
+    enableAllSteps: true,
+    transitionEffectSpeed: 500,
+    labels: {
+      finish: "Submit",
+      next: "Forward",
+      previous: "Backward"
+    }
+  });
+  $(".wizard > .steps li a").click(function() {
+    $(this)
+      .parent()
+      .addClass("checked");
+    $(this)
+      .parent()
+      .prevAll()
+      .addClass("checked");
+    $(this)
+      .parent()
+      .nextAll()
+      .removeClass("checked");
+  });
+  // Custome Jquery Step Button
+  $(".forward").click(function() {
+    $("#wizard").steps("next");
+  });
+  $(".backward").click(function() {
+    $("#wizard").steps("previous");
+  });
+  // Select Dropdown
+  $("html").click(function() {
+    $(".select .dropdown").hide();
+  });
+  $(".select").click(function(event) {
+    event.stopPropagation();
+  });
+  $(".select .select-control").click(function() {
+    $(this)
+      .parent()
+      .next()
+      .toggle();
+  });
+  $(".select .dropdown li").click(function() {
+    $(this)
+      .parent()
+      .toggle();
+    var text = $(this).attr("rel");
+    $(this)
+      .parent()
+      .prev()
+      .find("div")
+      .text(text);
+  });
+});
