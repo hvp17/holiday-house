6cd43dcacaed351365853fc81837af3b839b4be2
Tomas Gadliauskas <tgadliauskas@zendesk.com>
2019-05-21 15:40:41 +0200

    house api statuses change

--- server/routes/houses/index.js
+++ server/routes/houses/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
 const connection = require("../../connection");
 
 router.get("/", (req, res) => {
@@ -18,7 +17,7 @@ router.get("/", (req, res) => {
 router.get("/one/:id", (req, res) => {
   try {
     const { id } = req.params;
-    if (!id) res.send({ ok: 0, message: "No ID is provided" });
+    if (!id) res.send({ status: 0, message: "No ID is provided" });
     connection.execute(
       "SELECT * FROM houses WHERE id = ?",
       [id],
@@ -67,15 +66,11 @@ router.post("/create", (req, res) => {
         console.log(req.body);
         if (err || response.affectedRows < 1) {
           res.send({
-            ok: false,
-            msg: `Failed to register. ${err.message && err.message}`
+            status: 0,
+            message: `Failed to create house. ${err.message && err.message}`
           });
         }
-        // const token = jwt.sign(user.email, process.env.SECRET);
-
-        res.send({ ok: true });
-        if (err) throw err;
-        res.send(rows);
+        res.send({ status: 1 });
       }
     );
   } catch (error) {
@@ -87,14 +82,14 @@ router.post("/create", (req, res) => {
 router.get("/delete/:id", (req, res) => {
   try {
     const { id } = req.params;
-    if (!id) res.send({ ok: false, msg: "No ID is provided" });
+    if (!id) res.send({ status: 0, msg: "No ID is provided" });
     connection.execute("DELETE FROM houses WHERE id = ?", [id], (err, rows) => {
       if (err) throw err;
-      res.send({ ok: true, data: rows });
+      res.send({ status: 1, data: rows });
     });
   } catch (error) {
     console.log(error);
-    res.send({ ok: false, msg: error.msg });
+    res.send({ status: 0, msg: error.msg });
   }
 });
 
