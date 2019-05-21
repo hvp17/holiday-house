9996220016182d34e2258bae05b59eba9045ad49
tsmithdk <tsmithdk@gmail.com>
2019-05-21 13:59:11 +0200

    1

--- js/main/login.js
+++ js/main/login.js
@@ -1,13 +1,15 @@
 $(document).on('click', '#btnLogin', function () {
-    console.log('click');
-
     $.ajax({
-        url: 'apis/api-login.php',
+        url: "http://localhost:3000/user/login",
         data: $('#wizard').serialize(),
         method: 'POST',
         dataType: 'JSON'
     }).always(function (jData) {
         console.log("jData ", jData);
-
+        if(jData.status===1){
+            window.location.href = "/";
+        }else{
+            console.log('error login')
+        }
     })
 })
