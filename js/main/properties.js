5ceb0ef855b83216d5978e9e76a2e8934ef6fad6
Tomas Gadliauskas <tgadliauskas@zendesk.com>
2019-05-28 10:33:25 +0200

    show thumbnail in list

--- js/main/properties.js
+++ js/main/properties.js
@@ -1,18 +1,17 @@
-$(document).ready(function(){
-  
+$(document).ready(function() {
   function isFamilyFriendly(house) {
     if (house.family_friendly == 1) {
-      return 'Yes'
+      return "Yes";
     } else {
-      return 'No'
+      return "No";
     }
   }
 
   function isSmokerFriendly(house) {
     if (house.smoker_friendly == 1) {
-      return 'Yes'
+      return "Yes";
     } else {
-      return 'No'
+      return "No";
     }
   }
 
@@ -20,18 +19,35 @@ $(document).ready(function(){
     url: "http://localhost:3000/houses",
     dataType: "JSON"
   }).always(function(jData) {
-    let houses = jData.houses
+    let houses = jData.houses;
     houses.forEach(house => {
-      $('#housesContainer').append(`   
-        <div class="col-md-6 col-lg-4 mb-4 item" data-price="${house.price_per_night}">
+      $.ajax({
+        url: "http://localhost:3000/images/getHouseImages/" + house.id,
+        dataType: "JSON"
+      }).always(function(jImagesData) {
+        const { status, images } = jImagesData;
+        const firstThumbnail = images.find(x => x.path.includes("thumbnail"))
+          .path;
+        $("#housesContainer").append(`   
+        <div class="col-md-6 col-lg-4 mb-4 item" data-price="${
+          house.price_per_night
+        }">
           <div class="property-entry h-100">
-            <a href="property-details.php?id=${house.id}" class="property-thumbnail">
-              <img src="images/img_1.jpg" alt="Image" class="img-fluid">
+            <a href="property-details.php?id=${
+              house.id
+            }" class="property-thumbnail">
+              <img src="${firstThumbnail}" alt="Image" class="img-fluid">
             </a>
             <div class="p-4 property-body">
-              <h2 class="property-title"><a href="property-details.html">${house.title}</a></h2>
-              <span class="property-location d-block mb-3"><span class="property-icon icon-room"></span>${house.address}</span>
-              <strong class="property-price text-primary mb-3 d-block text-success">${house.price_per_night} DKK/night</strong>
+              <h2 class="property-title"><a href="property-details.html">${
+                house.title
+              }</a></h2>
+              <span class="property-location d-block mb-3"><span class="property-icon icon-room"></span>${
+                house.address
+              }</span>
+              <strong class="property-price text-primary mb-3 d-block text-success">${
+                house.price_per_night
+              } DKK/night</strong>
               <ul class="property-specs-wrap mb-3 mb-lg-0">
                 <li>
                   <span class="property-specs">Rooms</span>
@@ -39,71 +55,74 @@ $(document).ready(function(){
                 </li>
                 <li>
                   <span class="property-specs">Family friendly</span>
-                  <span class="familyFriendly" data-family="${house.family_friendly}">${isFamilyFriendly(house)}</span>
+                  <span class="familyFriendly" data-family="${
+                    house.family_friendly
+                  }">${isFamilyFriendly(house)}</span>
                 </li>
                 <li>
                   <span class="property-specs">Smoker friendly</span>
-                  <span class="smokerFriendly" data-smoker="${house.smoker_friendly}">${isSmokerFriendly(house)}</span>
+                  <span class="smokerFriendly" data-smoker="${
+                    house.smoker_friendly
+                  }">${isSmokerFriendly(house)}</span>
                 </li>
               </ul>
             </div>
           </div>
         </div>`);
+      });
     });
   });
 
-  let familyFriendlyFilter = document.getElementById('btnFamily')
+  let familyFriendlyFilter = document.getElementById("btnFamily");
 
-  familyFriendlyFilter.addEventListener('click', () => {
-    let smokerFriendlyElements = document.querySelectorAll('.smokerFriendly')
+  familyFriendlyFilter.addEventListener("click", () => {
+    let smokerFriendlyElements = document.querySelectorAll(".smokerFriendly");
     smokerFriendlyElements.forEach(smokerFriendlyElement => {
-      let value = smokerFriendlyElement.dataset.smoker
+      let value = smokerFriendlyElement.dataset.smoker;
       if (value == 0) {
-        let parent = smokerFriendlyElement.closest('.item')
+        let parent = smokerFriendlyElement.closest(".item");
         console.log(parent);
         $(parent).toggle();
       }
     });
-  })
+  });
 
-  let smokerFriendlyFilter = document.getElementById('btnSmoker')
+  let smokerFriendlyFilter = document.getElementById("btnSmoker");
 
-  smokerFriendlyFilter.addEventListener('click', () => {
-    let familyFriendlyElements = document.querySelectorAll('.familyFriendly')
+  smokerFriendlyFilter.addEventListener("click", () => {
+    let familyFriendlyElements = document.querySelectorAll(".familyFriendly");
     familyFriendlyElements.forEach(familyFriendlyElement => {
-      let value = familyFriendlyElement.dataset.family
+      let value = familyFriendlyElement.dataset.family;
       if (value == 0) {
-        let parent = familyFriendlyElement.closest('.item')
+        let parent = familyFriendlyElement.closest(".item");
         console.log(parent);
         $(parent).toggle();
       }
     });
-  })
+  });
 
-  $(document).on('change', '.select-price', function() {
+  $(document).on("change", ".select-price", function() {
     let sortingMethod = $(this).val();
-    if (sortingMethod == 'l2h') {
+    if (sortingMethod == "l2h") {
       sortProductsPriceAscending();
-    } else if (sortingMethod == 'h2l') {
+    } else if (sortingMethod == "h2l") {
       sortProductsPriceDescending();
     }
   });
 
   function sortProductsPriceAscending() {
-    let houses = $('.item');
-    houses.sort(function(a, b) { 
-      return $(a).data("price")-$(b).data("price")
+    let houses = $(".item");
+    houses.sort(function(a, b) {
+      return $(a).data("price") - $(b).data("price");
     });
-    $('#housesContainer').html(houses);
-    
+    $("#housesContainer").html(houses);
   }
 
   function sortProductsPriceDescending() {
-    let houses = $('.item');
-    houses.sort(function(a, b) { 
-      return $(b).data("price") - $(a).data("price")
+    let houses = $(".item");
+    houses.sort(function(a, b) {
+      return $(b).data("price") - $(a).data("price");
     });
-    $('#housesContainer').html(houses);
+    $("#housesContainer").html(houses);
   }
-    
 });
